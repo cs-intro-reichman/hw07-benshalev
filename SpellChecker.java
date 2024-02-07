8ff236dabd9c23651623b10c25a5d890126f865e
BenS <bshalev1@gmail.com>
2024-02-07 18:50:35 +0200

    homework

--- SpellChecker.java
+++ SpellChecker.java
@@ -12,10 +12,30 @@ public class SpellChecker {
 
 	public static String tail(String str) {
 		// Your code goes here
+		str = str.substring(1);
+		return str;
 	}
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
+		if (word1.length() == 0 || word2.length() == 0){
+			return(Math.max(word1.length(),word2.length()));
+		}
+
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		int dis = 0;
+
+		if(word1.charAt(0) == word2.charAt(0)){
+			levenshtein(tail(word1), tail(word2));
+		}
+
+		else{
+			dis++;
+			dis = dis + (int)(Math.min(levenshtein(tail(word1), tail(word2)),Math.min(levenshtein((word1), tail(word2)),levenshtein(tail(word1), tail(word2))))); 
+			}
+			return dis;
+
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -24,12 +44,22 @@ public class SpellChecker {
 		In in = new In(fileName);
 
 		// Your code here
+		for(int i = 0 ; i< 3000 ; i++){
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		String firshort = "";
+		for(int i = 0 ; i < 3000 ; i++){
+			if(levenshtein(word, dictionary[i]) <= threshold){
+				firshort = firshort + dictionary;
+			}
+			
 	}
-
+		return firshort;
+	}
 }
