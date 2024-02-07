5555881a2bd2e35e16267a69d0ff13147acc65a2
BenS <bshalev1@gmail.com>
2024-02-07 19:03:48 +0200

    homework

--- SpellChecker.java
+++ SpellChecker.java
@@ -5,7 +5,7 @@ public class SpellChecker {
 	public static void main(String[] args) {
 		String word = args[0];
 		int threshold = Integer.parseInt(args[1]);
-		String[] dictionary = readDictionary("dictionary.txt");
+		String []dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
 	}
