bf64c76c1f08fde1d6923078f73ded8aaee94102
BenS <bshalev1@gmail.com>
2024-02-07 19:27:48 +0200

    homework

--- SpellChecker.java
+++ SpellChecker.java
@@ -18,14 +18,15 @@ public class SpellChecker {
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
 		if (word1.isEmpty() || word2.isEmpty()){
 			return(Math.max(word1.length(),word2.length()));
 		}
 
-		word1 = word1.toLowerCase();
-		word2 = word2.toLowerCase();
-		if(word1.charAt(0) == word2.charAt(0)){
-			levenshtein(tail(word1), tail(word2));
+
+		else if(word1.charAt(0) == word2.charAt(0)){
+			return levenshtein(tail(word1), tail(word2));
 		}
 
 		else{
@@ -49,10 +50,13 @@ public class SpellChecker {
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
 		String firshort = "";
+		String smallerthenfirst = "";
 		for(int i = 0 ; i < 3000 ; i++){
 			if(levenshtein(word, dictionary[i]) <= threshold){
-				firshort = firshort + dictionary;
+				firshort = firshort + dictionary[i];
+
 			}
+
 			
 	}
 		return firshort;
